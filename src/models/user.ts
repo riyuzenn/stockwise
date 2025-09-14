0a612a2d88c0847cbea3349b6a4fb6ade5613fb1
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-15 00:17:46 +0800

    feat: user model

---
+++ src/models/user.ts
@@ -0,0 +1,9 @@
+
+import { Schema, model, models } from "mongoose"
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true })
+
+export const User = models.User || model("User", UserSchema);
