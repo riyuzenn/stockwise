74395b42b0231eb27e6372883bac0961638f861d
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-15 00:18:12 +0800

    feat: add mongo client

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,6 +1,30 @@
+// NOTE(bryle): i stole https://medium.com/@turingvang/next-js-beginner-mongodb-crud-example-tutorial-db2afdb68e25
+
 import mongoose from 'mongoose'
 
-const MONGO_URI = process.env.MONGO_URI
-if (!MONGO_URI) {
-  throw new Error('Please define mongo uri please')
+
+const MONGODB_URI = process.env.MONGO_URI as string
+
+if (!MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env.local")
+}
+
+let cached = (global as any).mongoose
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null }
+}
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "stockDb",
+      bufferCommands: false,
+    }).then((mongoose) => mongoose)
+  }
+
+  cached.conn = await cached.promise
+  return cached.conn
 }
