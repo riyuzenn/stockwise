3ce235d7f303601087208a67fe50037a83c8b9fd
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-15 00:21:10 +0800

    ref: add plasma wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import PlasmaWrapper from '@/components/plasma-wrapper'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +28,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
+          <PlasmaWrapper>{children}</PlasmaWrapper>
         </ThemeProvider>
       </body>
     </html>
