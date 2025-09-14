694c03f4c4b78b0598a21d9594beb4239d69c183
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-15 00:19:28 +0800

    feat: create logout api

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,15 @@
+
+import { NextResponse } from "next/server"
+
+export async function POST() {
+  const res = NextResponse.json({ message: "Logged out" })
+  res.cookies.set({
+    name: "session",
+    value: "",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    maxAge: 0, // expire immediately
+  })
+  return res
+}
