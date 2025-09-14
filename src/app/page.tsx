a4095ede370967a4208bbc8e1be65a2313d8c46c
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-15 00:21:40 +0800

    ref: login structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,14 @@
-'use client'
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+import LoginPage from "@/app/login-page"
 
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+export default async function HomePage() {
+  const cookieStore = await cookies()
+  const session = cookieStore.get("session")
 
-import React from 'react'
-import {
-  Form,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormMessage,
-} from '@/components/ui/form'
-
-const loginSchema = z.object({
-  username: z.string(),
-  password: z.string().min(6),
-})
-
-type LoginValues = z.infer<typeof loginSchema>
-
-export default function LoginPage() {
-  const router = useRouter()
-  const [loading, setLoading] = useState(false)
-
-  const form = useForm<LoginValues>({
-    resolver: zodResolver(loginSchema),
-    defaultValues: { username: '', password: '' },
-  })
-
-  const onSubmit = async (values: LoginValues) => {
-    setLoading(true)
-    try {
-      await new Promise((res) => setTimeout(res, 1000))
-      console.log('login success', values)
-
-      router.push('/dashboard')
-    } catch (err) {
-      console.error(err)
-    } finally {
-      setLoading(false)
-    }
+  if (session) {
+    redirect("/dashboard")
   }
-  return (
-    <React.Fragment>
-      <div className="flex min-h-[90vh] justify-center items-center">
-        <div className="">
-          <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
-            Log in to StockWise
-          </h1>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <div className="pt-16 flex flex-col gap-8">
-                <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input className="h-12 pl-4" placeholder="Enter your username" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
 
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="h-12 pl-4"
-                          type="password"
-                          placeholder="Enter your password"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <Button type="submit" className="w-full" disabled={loading}>
-                  {loading ? 'Logging in...' : 'Login'}
-                </Button>
-              </div>
-            </form>
-          </Form>
-        </div>
-      </div>
-    </React.Fragment>
-  )
+  return <LoginPage />
 }
