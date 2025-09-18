03e75aaa5d6732933fc985e43f5ed7942fc9c6f3
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2025-09-19 01:24:54 +0800

    feat: onboarding overview page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,26 +6,16 @@ import React from 'react'
 
 import { Switch } from '@/components/ui/switch'
 import LogoutButton from '@/components/ui/logout-button'
+import Header from '@/components/header'
+import OverviewPage from './overview-page'
 
 export default function Dashboard() {
   const { theme, setTheme } = useTheme()
 
   return (
     <React.Fragment>
-      <div className="flex justify-center items-center min-h-[90vh]">
-        <h1 className="font-semibold text-2xl dark:text-green-100 text-black">Dashboard Page</h1>
-        <Switch
-          checked={theme === 'dark' ? true : false}
-          onCheckedChange={(checked) => {
-            if (checked) {
-              setTheme('dark')
-            } else {
-              setTheme('light')
-            }
-          }}
-        />
-        <p>{theme === 'dark' ? 'Dark' : 'Light'}</p>
-      </div>
+      <Header />
+      <OverviewPage />
       <LogoutButton />
     </React.Fragment>
   )
