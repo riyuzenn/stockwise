fd4589a33745376ebc77b920465bcfee855b7902
riyuzenn <66673988+riyuzenn@users.noreply.github.com>
2026-01-07 01:06:52 +0800

    change color

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -105,7 +105,7 @@ export function RightSideBar() {
   //   URL.revokeObjectURL(url)
   // }
   return (
-    <div className="border-l border-black/10 dark:border-white/10 flex bg-[#fafafa] dark:bg-[#171717] md:flex-col md:w-40 md:px-4 md:sticky md:top-0 md:h-screen flex-row w-full px-6 py-4 sticky bottom-0 items-center justify-center gap-10 z-50">
+    <div className="border-l border-black/10 dark:border-white/10 flex bg-[#d3d3d3] dark:bg-[#171717] md:flex-col md:w-40 md:px-4 md:sticky md:top-0 md:h-screen flex-row w-full px-6 py-4 sticky bottom-0 items-center justify-center gap-10 z-50">
       <div className="flex flex-col items-center gap-3">
         <AddProductDialog />
         <p className="text-sm md:text-md">Add Product</p>
